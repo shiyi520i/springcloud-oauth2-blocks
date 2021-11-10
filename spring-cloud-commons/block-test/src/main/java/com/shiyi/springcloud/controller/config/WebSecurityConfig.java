c0a1ce52b09a935fa984952702070b5e2ff2d79c
shiyi <1434980328@qq.com>
2021-11-10 10:56:31 +0800

    fix bug

--- spring-cloud-commons/block-test/src/main/java/com/shiyi/springcloud/controller/config/WebSecurityConfig.java
+++ spring-cloud-commons/block-test/src/main/java/com/shiyi/springcloud/controller/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.shiyi.springcloud.config;
+package com.shiyi.springcloud.controller.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
